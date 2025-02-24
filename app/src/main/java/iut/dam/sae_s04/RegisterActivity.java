e1035c19ba77c735bb885a1e0f81acd5e6030f5b
alya1-yes <alyaayinde@gmail.com>
2025-02-24 22:40:48 +0100

    amelioration des boutons des pages

--- app/src/main/java/iut/dam/sae_s04/RegisterActivity.java
+++ app/src/main/java/iut/dam/sae_s04/RegisterActivity.java
@@ -1,24 +1,26 @@
 package iut.dam.sae_s04;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class RegisterActivity extends AppCompatActivity {
+
+public class RegisterActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainLayout), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        //Barre de navigation
+        Button walletButton = findViewById(R.id.wallet_button);
+        Button homeButton = findViewById(R.id.home_button);
+        Button addRingButton = findViewById(R.id.add_ring_button);
+        Button userButton = findViewById(R.id.user_button);
+
+
+        navigateToActivity(walletButton, ResumeActivity.class);
+        navigateToActivity(homeButton, AcceuilActivity.class);
+        navigateToActivity(addRingButton, RegisterActivity.class);
+        navigateToActivity(userButton, LoginActivity.class);
     }
 }
